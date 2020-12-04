6a61f510e021d79177390c9c6a36532d4dc475a2
rfm50 <robertfmath@gmail.com>
2020-12-04 11:56:12 -0500

    made simulator a function instead

--- driftSimulation.py
+++ driftSimulation.py
@@ -1,56 +1,45 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-# Set parameters:
 
-NeSmall = 10
-NeMedium = 100
-NeLarge = 1000
-replicates = 10
-gens = 100
-startingFreq = 0.5
+def drift_simulator(NeSmall=10, NeMedium=100, NeLarge=1000, replicates=10,
+                    gens=100, startingFreq=0.5):
 
-# Simulation:
+    fig, axs = plt.subplots(1, 3, figsize=(20, 5))
 
-fig, axs = plt.subplots(1, 3, figsize=(20, 5))
+    for ind, twoN in enumerate([2*NeSmall, 2*NeMedium, 2*NeLarge]):
+        
 
-for ind, twoN in enumerate([2*NeSmall, 2*NeMedium, 2*NeLarge]):
-    
-    # These next 3 lines are just so all the variables can be defined in one place--at the top.
-    replicates = replicates 
-    gens = gens 
-    startingFreq = startingFreq
-    
-    pops = []
-    for replicate in np.arange(replicates):
-        p = startingFreq
-        q = 1-p
+        pops = []
+        for replicate in np.arange(replicates):
+            p = startingFreq
+            q = 1-p
 
-        freqs = [p]
+            freqs = [p]
 
-        for generation in np.arange(gens):
-            if p >= 0.995 or p <= 0.005:
-                freqs.append(round(p))
-            else:
-                newP = (np.random.binomial(twoN, p))/twoN
-                if newP > 1 or newP < 0:
+            for generation in np.arange(gens):
+                if p >= 0.995 or p <= 0.005:
                     freqs.append(round(p))
-                    p = newP
                 else:
-                    freqs.append(newP)
-                    p = newP
+                    newP = (np.random.binomial(twoN, p))/twoN
+                    if newP > 1 or newP < 0:
+                        freqs.append(round(p))
+                        p = newP
+                    else:
+                        freqs.append(newP)
+                        p = newP
 
-        pops.append(freqs)
-        p = startingFreq
+            pops.append(freqs)
+            p = startingFreq
 
-    for pop in pops:
-        axs[ind].plot(pop)
+        for pop in pops:
+            axs[ind].plot(pop)
 
-    axs[ind].set_ylabel('Frequency of $p$')
-    axs[ind].set_xlabel('Generations')
-    axs[ind].set_ylim((0, 1))
-    axs[ind].set_xlim((0, gens))
-    axs[ind].set_title(f'2N = {twoN}')
+        axs[ind].set_ylabel('Frequency of $p$')
+        axs[ind].set_xlabel('Generations')
+        axs[ind].set_ylim((0, 1))
+        axs[ind].set_xlim((0, gens))
+        axs[ind].set_title(f'2N = {twoN}')
 
 
-plt.show()
+    plt.show()
